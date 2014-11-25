f5469c26133efebf70fed0c1c175eafa47663978
Luke Bartel <bartelluke@gmail.com>
2014-11-24 20:24:12 -0700

    added location count to people index

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,18 +1,39 @@
 class AssignmentsController < ApplicationController
 
-  def new
+  before_action do
     @person = Person.find(params[:person_id])
+  end
+
+  def new
     @assignment = @person.assignments.new
   end
 
   def create
-    @person = Person.find(params[:person_id])
     @assignment = @person.assignments.new(params.require(:assignment).permit(:location_id, :role))
     if @assignment.save
       redirect_to @person
     else
       render :new
     end
+  end
+
+  def edit
+    @assignment = @person.assignments.find(params[:id])
+  end
+
+  def update
+    @assignment = @person.assignments.find(params[:id])
+    if @assignment.update(params.require(:assignment).permit(:location_id, :role))
+      redirect_to @person
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @assignment = @person.assignments.find(params[:id])
+    @assignment.destroy
+    redirect_to person_path(@person)
   end
 
 end
