448192235580ba2bc9cf754b5ff65688905e5c94
Jeff Dean <jeff@galvanize.it>
2014-11-24 22:28:36 -0700

    updated data to always have ids, but for them to be wrong a third of the time

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,57 +10,33 @@ User.create!(
   password_confirmation: 'password'
 )
 
-Person.create!(
-  title: "Mr",
-  first_name: "Joe",
-  last_name: "Example"
-)
-
-10.times do
+people = 30.times.map do
   title = Faker::Name.prefix
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   Person.create!(
-  {first_name: first_name,
-  last_name: last_name,
-  title: title}
+    first_name: first_name,
+    last_name: last_name,
+    title: title,
   )
 end
 
-10.times do
-  name = Faker::Name.name
-  email = Faker::Internet.email
-  User.create!({
-    name: name,
-    email: email,
-    password: 'password',
-    password_confirmation: 'password'
-    })
-end
-
-10.times do
+locations = 15.times.map do
   name = Faker::Address.state
-  Location.create!({
-    name: name
-    })
-end
-
-5.times do
-  role = Faker::Name.title
-  Assignment.create!({
-    role: role
-    })
-end
-
-20.times do
-  role = Faker::Name.title
-  Assignment.create!({
-    role: role,
-    location_id: rand(20),
-    person_id: rand(20)
-  })
-end
-
-["Northeast", "Midwest", "Mountain West", "Northwest", "Southwest", "Deep south", "Texas"].each do |name|
   Location.create!(name: name)
+end
+
+max_person_id = Person.maximum(:id)
+person_ids = people.map(&:id) + ((max_person_id + 1)..(max_person_id + 20)).to_a
+
+max_location_id = Location.maximum(:id)
+location_ids = locations.map(&:id) + ((max_location_id + 1)..(max_location_id + 10)).to_a
+
+100.times do
+  role = Faker::Name.title
+  Assignment.create!(
+    role: role,
+    location_id: location_ids.sample,
+    person_id: person_ids.sample
+  )
 end
